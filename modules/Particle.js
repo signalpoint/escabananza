7a026b0a889645fb34fa348069aab82036c31c3c
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2022-12-26 15:20:59 -0500

    On Particle, add fades and opacity properties, then draw() and update() them accordingly

--- modules/Particle.js
+++ modules/Particle.js
@@ -1,6 +1,6 @@
 class Particle {
 
-  constructor({ position, velocity, radius, color = 'blue', snowball = false }) {
+  constructor({ position, velocity, radius, color = 'blue', snowball = false, fades = false }) {
 
     this.position = {
       x: position.x,
@@ -20,14 +20,20 @@ class Particle {
 
     this.ttl = 300
 
+    this.opacity = 1
+    this.fades = fades
+
   }
 
   draw() {
+    c.save()
+    c.globalAlpha = this.opacity
     c.beginPath()
     c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false)
     c.fillStyle = this.color
     c.fill()
     c.closePath()
+    c.restore()
   }
 
   update() {
@@ -41,6 +47,14 @@ class Particle {
 
     if (this.position.y + this.radius + this.velocity.y <= canvas.height) { // above bottom
       this.velocity.y += gravity * .4
+    }
+
+    if (this.fades && this.opacity > 0) {
+      this.opacity -= .01
+    }
+
+    if (this.opacity < 0) {
+      this.opacity = 0
     }
 
   }
