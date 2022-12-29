4c187790f69b5f156d8a4e1978b6ab0cd6bb17d0
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2022-12-29 12:32:18 -0500

    Create a levels const for laying out the map, and adjust loadLevel() to chain promises by returning them

--- levels.js
+++ levels.js
@@ -1,67 +1,128 @@
 import { images } from './images.js'
 import { audio } from './audio.js'
 import { loadImages, loadAudio } from './media.js'
+import { canvas } from './canvas.js'
+import { getGame, getPlatforms } from './environment.js'
+import { Platform } from './modules/Platform.js'
 
-const levelsPlatformMap = {
-  1: [
-    'lg',
-    'lg',
-    'gap',
-    'lg',
-    'lg',
-    'gap',
-    'gap',
-    'lg',
-    'lg',
-    'gap',
-    'tall',
-    'tall',
-    'gap',
-    'gap',
-    'lg',
-    'tall',
-    'tall',
-    'gap',
-    'xtall',
-    'xtall',
-    'gap',
-    'gap',
-    'lg',
-    'gap',
-    'tall',
-    'tall',
-    'gap',
-    'gap',
-    'lg',
-    'tall',
-    'tall',
-    'gap',
-    'xtall',
-    'xtall',
-    'gap',
-    'gap',
-    'lg',
-    'lg',
-    'lg'
-  ],
-  2: [
-    'lg',
-    'gap',
-    'tall',
-    'gap',
-    'xtall'
-  ]
+const levels = {
+  1: {
+    platforms: [],
+    map: [
+      'lg',
+      'lg',
+      'gap',
+      'lg',
+      'lg',
+      'gap',
+      'gap',
+      'lg',
+      'lg',
+      'gap',
+      'tall',
+      'tall',
+      'gap',
+      'gap',
+      'lg',
+      'tall',
+      'tall',
+      'gap',
+      'xtall',
+      'xtall',
+      'gap',
+      'gap',
+      'lg',
+      'gap',
+      'tall',
+      'tall',
+      'gap',
+      'gap',
+      'lg',
+      'tall',
+      'tall',
+      'gap',
+      'xtall',
+      'xtall',
+      'gap',
+      'gap',
+      'lg',
+      'lg',
+      'lg'
+    ]
+  },
+  2: {
+    platforms: [],
+    map: [
+      'lg',
+      'gap',
+      'tall',
+      'gap',
+      'xtall'
+    ]
+  }
 }
 
 function loadLevel(number) {
 
+
   return loadImages(images.levels[number]).then(function() {
 
-    console.log('loaded images', number);
+    console.log('loaded images for level', number)
 
-    loadAudio(audio.levels[number]).then(function() {
+    return loadAudio(audio.levels[number]).then(function() {
 
-      console.log('loaded audio', number);
+      console.log('loaded audio for level', number)
+
+      let game = getGame()
+
+      switch (number) {
+
+        case 1:
+
+          // level platforms
+          getPlatforms().push(
+
+            // tall platform
+            new Platform({
+              x: 1230 + images.levels[game.level].platform.width - images.levels[game.level].platformTall.width,
+              y: canvas.height - images.levels[game.level].platform.height - images.levels[game.level].platformTall.height * .75,
+              image: images.levels[game.level].platformTall
+            })
+
+          )
+
+        // tri block + single block stack
+    //    new Platform({
+    //      x: 1900 + 128,
+    //      y: 350,
+    //      image: assets.triBlock,
+    //      block: true
+    //    }),
+    //    new Platform({
+    //      x: 1900 + 128 + assets.singleBlock.width,
+    //      y: 225,
+    //      image: assets.singleBlock,
+    //      block: true
+    //    }),
+
+
+
+        // 2 single blocks, stacked (blocks their path, forces them to jump over)
+    //    new Platform({
+    //      x: 1230 + (assets.platform.width / 2),
+    //      y: platformDefaultY - assets.singleBlock.height,
+    //      image: assets.singleBlock,
+    //      block: true
+    //    }),
+    //    new Platform({
+    //      x: 1230 + (assets.platform.width / 2),
+    //      y: platformDefaultY - assets.singleBlock.height * 2,
+    //      image: assets.singleBlock,
+    //      block: true
+    //    })
+
+          break
+      }
 
     });
 
@@ -71,7 +132,7 @@ function loadLevel(number) {
 
 export {
 
-  levelsPlatformMap,
+  levels,
   loadLevel
 
 }
