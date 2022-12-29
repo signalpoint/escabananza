c2af18add5dd2ac0ea14917ec815f4d9d08256ac
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2022-12-28 22:00:57 -0500

    Move init() into the canvas.js module. Use new functions from media.js to load assets and start level 1

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ import { images } from './images.js'
 import { audio } from './audio.js'
 import { playSound, playMusic, stopMusic, loadImages, loadAudio } from './media.js'
 
-import { canvas, c } from './canvas.js'
+import {
+  canvas,
+  c,
+  init
+} from './canvas.js'
+
 import {
   getGame,
   setGame,
-  gravity,
+  getScrollOffset,
+  setScrollOffset,
+  scrollOffsetFinish,
+  setGravity,
+  getGravity,
   getPlayer,
   setPlayer,
   getPlatforms,
@@ -31,7 +40,7 @@ import {
   isCircleOnTopOfPlatform
 } from './utilities/collision.js'
 
-import { Layer } from './modules/Layer.js'
+//import { Layer } from './modules/Layer.js'
 import { Background } from './modules/Background.js'
 import { Foreground } from './modules/Foreground.js'
 import { GenericObject } from './modules/GenericObject.js'
@@ -59,17 +68,11 @@ let gameAnimationFrame;
 //  stopGame();
 //}, 60000);
 
-// CONSTANTS
-
-// Gravity
-//let gravity = 0.5
-
 // GAME PARTS...
 
-// PLAYER
-
-let player = getPlayer()
+let player = null // not available until after init()
 let game = getGame()
+let scrollOffset = getScrollOffset()
 let platforms = getPlatforms()
 let genericObjects = getGenericObjects()
 let backgrounds = getBackgrounds()
@@ -77,7 +80,7 @@ let foregrounds = getForegrounds()
 let badbushes = getBadBushes()
 let particles = getParticles()
 let snowflowers = getSnowFlowers()
-let flagPole = getFlagPole()
+let flagPole = null // not available until after init()
 
 // HELPERS
 
@@ -108,354 +111,18 @@ const keys = {
   }
 }
 
-// SCROLL OFFSET
-
-let scrollOffset = 0
-
-// WIN
-const scrollOffsetFinish = 7650
-//const scrollOffsetFinish = 420 // test
-
-// INIT
-
-var init = function() {
-
-  game = {
-    disableUserInput: false,
-    musicEnabled: true,
-    level: 1
-  }
-
-  setGame(game)
-
-  var platformWidth = images.levels[game.level].platform.width
-  var platformDefaultY = canvas.height - images.levels[game.level].platform.height
-
-  // GAME PARTS...
-
-  // PLAYER
-
-  player = new Player({
-    position: {
-      x: 100,
-      y: platformDefaultY - images.player.standRight.height // start on the ground
-    },
-    velocity: {
-      x: 0,
-      y: 0
-    }
-  })
-
-  setPlayer(player)
-
-  // PLATFORMS
-
-  // Place generic in-game platforms first...
-
-  platforms.push(
-
-    // tri block + single block stack
-//    new Platform({
-//      x: 1900 + 128,
-//      y: 350,
-//      image: assets.triBlock,
-//      block: true
-//    }),
-//    new Platform({
-//      x: 1900 + 128 + assets.singleBlock.width,
-//      y: 225,
-//      image: assets.singleBlock,
-//      block: true
-//    }),
-
-    // tall platform
-    new Platform({
-      x: 1230 + images.levels[game.level].platform.width - images.levels[game.level].platformTall.width,
-      y: canvas.height - images.levels[game.level].platform.height - images.levels[game.level].platformTall.height * .75,
-      image: images.levels[game.level].platformTall
-    })
-
-    // 2 single blocks, stacked (blocks their path, forces them to jump over)
-//    new Platform({
-//      x: 1230 + (assets.platform.width / 2),
-//      y: platformDefaultY - assets.singleBlock.height,
-//      image: assets.singleBlock,
-//      block: true
-//    }),
-//    new Platform({
-//      x: 1230 + (assets.platform.width / 2),
-//      y: platformDefaultY - assets.singleBlock.height * 2,
-//      image: assets.singleBlock,
-//      block: true
-//    })
-
-  )
-
-  // Build level platforms map...
-
-  const platformsMap = [
-    'lg',
-    'lg',
-    'gap',
-    'lg',
-    'lg',
-    'gap',
-    'gap',
-    'lg',
-    'lg',
-    'gap',
-    'tall',
-    'tall',
-    'gap',
-    'gap',
-    'lg',
-    'tall',
-    'tall',
-    'gap',
-    'xtall',
-    'xtall',
-    'gap',
-    'gap',
-    'lg',
-    'gap',
-    'tall',
-    'tall',
-    'gap',
-    'gap',
-    'lg',
-    'tall',
-    'tall',
-    'gap',
-    'xtall',
-    'xtall',
-    'gap',
-    'gap',
-    'lg',
-    'lg',
-    'lg'
-  ]
-
-  // Place level platforms onto collection...
-
-  let platformDistance = 0
-
-  platformsMap.forEach(symbol => {
-    switch (symbol) {
-
-      case 'lg':
-        platforms.push(new Platform({
-          x: platformDistance,
-          y: canvas.height - images.levels[game.level].platform.height,
-          width: platformWidth,
-          image: images.levels[game.level].platform,
-//          block: true, // causes hitSideOfPlatform() to trigger upon game start, i.e. can't move, x velocity always 0
-          text: platformDistance
-        }))
-        platformDistance += platformWidth
-        break;
-
-      case 'gap':
-        platformDistance += 150
-        break;
-
-      case 'tall':
-        platforms.push(new Platform({
-          x: platformDistance,
-          y: canvas.height - images.levels[game.level].platformTall.height,
-          image: images.levels[game.level].platformTall,
-          text: platformDistance
-        }))
-        platformDistance += images.levels[game.level].platformTall.width // hmmm?
-        break;
-
-      case 'xtall':
-        platforms.push(new Platform({
-          x: platformDistance,
-          y: canvas.height - images.levels[game.level].platformXTall.height,
-          image: images.levels[game.level].platformXTall,
-          text: platformDistance
-        }))
-        platformDistance += images.levels[game.level].platformXTall.width // hmmm?
-        break;
-
-    }
-
-  })
-
-  // 6 - block
-//  platforms.push(new Platform({
-//    x: platforms[0].position.x + Math.round(platforms[0].width*.25),
-//    y: platformDefaultY - 108,
-//    width: 96,
-//    image: assets.triBlock,
-//    block: true
-//  }));
-
-  // FLAG POLE
-
-  flagPole = new GenericObject({
-    x: scrollOffsetFinish,
-    y: canvas.height - images.levels[game.level].platform.height - images.generic.flagPole.height,
-    image: images.generic.flagPole,
-    scrollSpeed: 1
-  })
-
-  setFlagPole(flagPole)
-
-  // GENERIC OBJECTS
-
-  const oakTreeDefaultY = canvas.height - images.levels[game.level].platform.height - images.trees.oakTree.height
-
-  genericObjects.push(
-
-    // oak tree
-    new GenericObject({
-      x: 420,
-      y: oakTreeDefaultY,
-      image: images.trees.oakTree
-    }),
-
-    // oak tree
-    new GenericObject({
-      x: 1950,
-      y: oakTreeDefaultY,
-      image: images.trees.oakTree
-    }),
-
-    // oak tree
-    new GenericObject({
-      x: 2400,
-      y: oakTreeDefaultY,
-      image: images.trees.oakTree
-    })
-
-  )
-
-  // BACKGROUNDS
-
-  backgrounds.push(
-
-    // SKY
-    new Background({
-      x: 0,
-      y: 0,
-      image: images.levels[game.level].sky,
-      scrollSpeed: .11,
-      autoScroll: true
-    }),
-
-    // HILLS
-    new Background({
-      x: 0,
-      y: 0,
-      image: images.levels[game.level].hills,
-      scrollSpeed: .33
-    })
-
-    // OAK TREE
-//    new Background({
-//      x: 420,
-//      y: canvas.height - assets.platform.height - assets.oakTree.height,
-//      image: assets.oakTree
-//    })
-
-  )
-
-  // BAD BUSHES
-
-  badbushes.push(
-
-    new BadBush({
-      position: {
-        x: 650,
-        y: platformDefaultY - 100
-      },
-      velocity: {
-        x: -1,
-        y: 0
-      }
-    }),
-
-    new BadBush({
-      position: {
-        x: 2000,
-        y: platformDefaultY - 100
-      },
-      velocity: {
-        x: -1,
-        y: 0
-      }
-    }),
-
-    new BadBush({
-      position: {
-        x: 4000,
-        y: platformDefaultY - images.levels[game.level].platformTall.height
-      },
-      velocity: {
-        x: -1,
-        y: 0
-      }
-    }),
-
-    new BadBush({
-      position: {
-        x: 5500,
-        y: platformDefaultY - images.levels[game.level].platformTall.height
-      },
-      velocity: {
-        x: -1,
-        y: 0
-      }
-    })
-
-  )
-
-  // PARTICLES
-//  particles.push()
-
-  // SNOW FLOWERS
-
-  snowflowers.push(
-
-    new SnowFlower({
-      position: {
-        x: 420,
-        y: 96
-      },
-      velocity: {
-        x: 0,
-        y: 0
-      }
-    })
-
-  )
-
-  // FOREGROUNDS
-
-//  foregrounds.push(
-
-    // BIRCH TREE
-//    new Foreground({
-//      x: platforms[0].position.x + platforms[0].width *.9,
-//      y: platforms[0].position.y - assets.birchTree.height,
-//      image: assets.birchTree
-//    })
-
-//  )
-
-  scrollOffset = 0
-
-}
-
 // ANIMATE
 
 function animate() {
 
   gameAnimationFrame = requestAnimationFrame(animate)
 
-//  c.fillStyle = 'white'
-//  c.fillRect(0, 0, canvas.width, canvas.height)
+  game = getGame()
+  player = getPlayer()
+
+  scrollOffset = getScrollOffset()
+
+  flagPole = getFlagPole()
 
   // Draw Backgrounds
   backgrounds.forEach(background => {
@@ -514,7 +181,7 @@ function animate() {
       player.velocity.x = 0
       player.velocity.y = 0
 
-      gravity = 0
+      setGravity(0)
 
       player.sprite = player.sprites.stand.right
 
@@ -643,6 +310,7 @@ function animate() {
       }
       else if (!player.invincible) {
 
+        console.log('hit');
         player.die()
 
       }
@@ -707,6 +375,7 @@ function animate() {
       if (!hitSide) {
 
         scrollOffset += player.speed
+        setScrollOffset(scrollOffset)
 
         backgrounds.forEach(background => {
           background.velocity.x = -player.speed * background.scrollSpeed // parallax scroll
@@ -761,6 +430,7 @@ function animate() {
       if (!hitSide) {
 
         scrollOffset -= player.speed
+        setScrollOffset(scrollOffset)
 
         backgrounds.forEach(background => {
           background.velocity.x = player.speed * background.scrollSpeed // parallax scroll
@@ -986,6 +656,7 @@ function animate() {
 
   // Fall below canvas.
   if (player.position.y > canvas.height) {
+    console.log('fall');
     player.die()
   }
 
@@ -999,6 +670,8 @@ function start() {
   // INIT
 
   init()
+
+  player = getPlayer()
 
   // ANIMATE
 
@@ -1185,71 +858,3 @@ loadImages([
   })
 
 })
-
-//})
-
-//var assetsToLoad = totalAssets
-//
-//for (var name in assets) {
-//
-//  if (!assets.hasOwnProperty(name)) { continue }
-//
-//  var src = assets[name].src
-//  var filename = src.split('/').pop()
-//
-//  // Get file extension. @credit https://stackoverflow.com/a/1203361/763010
-//  var type = filename.substring(filename.lastIndexOf('.')+1, filename.length) || filename;
-//
-//  switch (type) {
-//
-//    case 'png':
-//
-//      var img = new Image()
-//      img.src = src
-//      img.setAttribute('data-name', name)
-//
-//      img.onload = function() {
-//
-//        var name = this.getAttribute('data-name')
-//        console.log(`loaded ${name}`, this.src)
-//
-//        assets[name] = this
-//        assetsToLoad--
-//        if (!assetsToLoad) start()
-//
-//      }
-//
-//      break
-//
-//    case 'mp3':
-//    case 'wav':
-//
-//      var sound = new Audio(src)
-//      sound.setAttribute('data-name', name)
-//
-//      sound.addEventListener("canplay", (event) => {
-//
-//        // The canplay event fires each time we play a sound, so we skip it once all assets are loaded.
-//        if (!assetsToLoad) return
-//
-//        var self = event.target
-//        var name = self.getAttribute('data-name')
-//        console.log('loaded', name, self.src)
-//
-//        assets[name] = self
-//        assetsToLoad--
-//        if (!assetsToLoad) start()
-//
-//      });
-//
-//      break
-//
-//    default:
-//
-//      console.error(`${src} can't be loaded`);
-//
-//      break
-//
-//  }
-//
-//}
